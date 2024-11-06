dd92a36484de48d606a015f23b2ae71b65a1ae1d
Akshayavaruni-2024-aia <varuniakshaya594@gmail.com>
2024-11-06 21:08:14 +0530

    Add files via upload

---
+++ portfolio1.py
@@ -0,0 +1,16 @@
+import streamlit as st 
+def portfolio1():
+ st.title("Akshaya varuni's Portfolio")
+
+ st.title("ABOUT ME")
+ st.write("I'm Akshaya varuni, from 1st year B.Tech Artificial Intelligence And Data Science")
+
+ st.title("AMBITION")
+ st.write("To Become a Data Scientist")
+ st.title("INTEREST")
+ st.write("Like to some problem")
+ st.write("To develop skills")
+ st.write("App Developing")
+ st.write("More interested in music")
+ st.title("CONTACT")
+ st.write("Email: varuniakshaya594@gmail.com")
